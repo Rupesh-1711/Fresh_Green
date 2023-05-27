aed7801f1a2467e1d51207785eff63bb2958d60d
Rupesh Patil <101388825+Rupesh-1711@users.noreply.github.com>
2023-05-27 17:05:48 +0530

    new project

---
+++ FreshGreen/src/main/java/com/freshgreen/FreshGreenApplication.java
@@ -0,0 +1,13 @@
+package com.freshgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshGreenApplication.class, args);
+	}
+
+}
---
+++ FreshGreen/src/test/java/com/freshgreen/FreshGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshgreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
